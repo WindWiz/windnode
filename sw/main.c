7920beb8b2a46635372292222bc20bb3847d09be
Magnus Olsson <magnus@minimum.se>
2011-02-03 22:08:54 +0100

    GPLv3

--- sw/main.c
+++ sw/main.c
@@ -1,23 +1,43 @@
+/* Copyright (C) 2010-2011 Magnus Olsson
+ * 
+ * This file is part of Windnode
+ * Windnode is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Windnode is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Windnode.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include <avr/io.h>
 #include <util/delay.h>
 #include <avr/interrupt.h>
 #include <stdlib.h>
 
 #include "swuart.h"
+#include "uart.h"
 
 static FILE debug;
+static FILE gsm;
 
 int main(void)
 {
 	swuart_init(SWUART_BAUD(CONFIG_DEBUG_BAUDRATE, F_CPU), &debug);
+	uart_init(UART_BAUD(CONFIG_GSM_BAUDRATE, F_CPU), &gsm, NULL);
+
 	stdout = &debug;
 	stdin = &debug;
 
 	sei();
 
-	DDRB |= _BV(PB1);
-	PORTB |= _BV(PB1);
 	while (1) {
+
 	}
 
 	return 0;
--- sw/swuart.c
+++ sw/swuart.c
@@ -1,3 +1,20 @@
+/* Copyright (C) 2010-2011 Magnus Olsson
+ * 
+ * This file is part of Windnode
+ * Windnode is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Windnode is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Windnode.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 /* SWUART - A software USART implementation.
  * Stdio interface to a software 8N1 UART port. The implementation is full 
  * duplex and driven by the two 8-bit timers (Timer #0 & #2) found on the 
