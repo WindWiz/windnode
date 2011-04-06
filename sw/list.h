94169bab534dc8534721c710660e9ad6c0e6dac9
Magnus Olsson <magnus@minimum.se>
2011-04-06 22:12:38 +0200

    Add safe iterator macro
    The safe iterator allows removing item during iteration.

--- sw/list.h
+++ sw/list.h
@@ -55,6 +55,10 @@ static inline uint8_t list_empty(struct list_head *head)
 #define list_for_each(pos, head) \
 	for (pos = (head)->next; pos != (head); pos = pos->next)
 
+#define list_for_each_safe(pos, n, head) \
+	for (pos = (head)->next, n = pos->next; pos != (head); \
+		pos = n, n = pos->next)
+
 #define list_entry(ptr, type, member) \
 	((type *)((char *)(ptr)-(unsigned long)(&((type *)0)->member)))
 
