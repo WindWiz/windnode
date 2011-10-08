bcbef829979dd84a55dd8d6d461046cd27acf642
Magnus Olsson <magnus@minimum.se>
2011-10-08 10:16:45 +0200

    Adjust formatting rules to give a bit more line width
    Seems to be a bug in GNU indent, -l80 actually breaks at 76.

--- sw/indent.sh
+++ sw/indent.sh
@@ -2,4 +2,5 @@
 # This is the Linux kernel coding style with a few exceptions:
 # -nbbo instead of -bbo, prefer breaking long lines AFTER boolean operators
 # -cdw, cuddle do-while
-indent -nbad -bap -nbc -nbbo -cdw -hnl -br -brs -c33 -cd33 -ncdb -ce -ci4 -cli0 -d0 -di1 -nfc1 -i8 -ip0 -l80 -lp -npcs -nprs -npsl -sai -saf -saw -ncs -nsc -sob -nfca -cp33 -ss -ts8 -il1 $*
+# -nlp, don't line up parentheses
+indent -nbad -bap -nbc -nbbo -cdw -hnl -br -brs -c33 -cd33 -ncdb -ce -ci4 -cli0 -d0 -di1 -nfc1 -i8 -ip0 -l85 -lp -npcs -nprs -npsl -sai -saf -saw -ncs -nsc -sob -nfca -cp33 -ss -ts8 -il1 -nlp $*
